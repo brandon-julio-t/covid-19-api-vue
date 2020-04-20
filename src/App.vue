52ac07cb612798ce9b25d4d5238b9a625eb5a5da
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-04-20 18:44:26 +0700

    Done summary page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-app-bar app>
+            <v-row justify="center">
+                <nav-item to="Summary" :exact="true" />
+            </v-row>
+        </v-app-bar>
+
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+
+        <v-footer app>
+            <!-- -->
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import NavItem from "./components/NavItem"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "App",
+
+        components: {
+            NavItem,
+        },
+
+        data: () => ({
+            //
+        }),
+    }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-btn :to="{ name: to }" :exact="exact" text>{{ to }}</v-btn>
+</template>
+
+<script>
+    export default {
+        name: "NavItem",
+        props: ["to", "exact"],
+    }
+</script>
+
+<style scoped></style>
